78177f479647cd34d057a2b12c1b213eb3da9099
mrobinson0207 <michael.robinson@industrieit.com>
2015-12-21 15:55:56 +1100

    Add project file update
    Add project file update

---
+++ Obsolete/Payout.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Resolvers;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class payoutResp
+{
+    [XmlRoot("payout"), Serializable]
+    public class payout
+    {
+        [XmlElement("response")]
+        public string response { get; set; }
+        [XmlElement("responsecode")]
+        public string responsecode { get; set; }
+        [XmlElement("responsetext")]
+        public string responsetext { get; set; }
+        [XmlElement("trans_id")]
+        public string trans_id { get; set; }
+        [XmlElement("ukash_voucher")]
+        public ukash_voucher ukash_voucher { get; set; }
+    }
+
+    public class ukash_voucher
+    {
+        [XmlElement("number")]
+        public string number { get; set; }
+        [XmlElement("currency")]
+        public string currency { get; set; }
+        [XmlElement("amount")]
+        public string amount { get; set; }
+        [XmlElement("expirydate")]
+        public string expirydate { get; set; }
+    }
+
+    public payoutResp()
+    {}
+}
