fde797c6867f3c77343f634cf9ecb4f957e4cb94
Parry Lee <parry.lee@industrieit.com>
2015-12-23 13:47:16 +1100

    create ApiManager and SearchRequest classes

---
+++ APILibrary/ApiManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using APILibrary.Models;
+
+namespace APILibrary
+{
+    public class ApiManager
+    {
+        private string managerId;
+        private string apiToken;
+
+        public ApiManager(string managerId, string apiToken)
+        {
+            this.managerId = managerId;
+            this.apiToken = apiToken;
+        }
+
+        public SearchRequest GetSearchResquest()
+        {
+            SearchRequest searchRequest = new SearchRequest(managerId, apiToken);
+            return searchRequest;
+        }
+    }
+}
---
+++ APILibrary/Models/SearchRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APILibrary.Models
+{
+    public class SearchRequest
+    {
+        public SearchRequest(string managerId, string apiToken)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
